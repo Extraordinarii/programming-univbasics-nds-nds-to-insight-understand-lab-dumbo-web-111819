484f1409ccdff7ab192027e90aee6122c2f6ab7b
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-11-15 20:13:07 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,5 +9,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  pp 
+  ss_movies = directors_database[0][:movies]
+  index = 0
+
+  while index < ss_movies.length do
+    titles = ss_movies[index][:title]
+    puts titles
+    index +=1
+  end
 end
